1bb0a9c897ff22c114c41918bd4f28c798731fc6
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2023-04-23 13:28:41 -0400

    Update MoveRobot.ino
    Added customization for travelling forwards/backwards and the angle for turns

--- MoveRobot.ino
+++ MoveRobot.ino
@@ -2,61 +2,79 @@
 int STBY = 3;
 
 // Right Wheels
-int PWMA = 5; // speed of right motors
-int AIN1 = 7; // direction of right motors
+int PWMA = 5;
+int AIN1 = 7;
 
 // Left Wheels
-int PWMB = 6; // speed of left motors
-int BIN1 = 8; // direction of left motors
+int PWMB = 6;
+int BIN1 = 8;
 
-int delayTime = 500;
+float t; // time in milliseconds
+float rate = 2.45; // rate of linear movement = 2.45ft/sec
+float w = 398; // angular velocity = 398 degrees/sec
 
 void setup() {
   // initialize 
-  pinMode(STBY, OUTPUT); 
+  pinMode(STBY, OUTPUT);
   pinMode(PWMA, OUTPUT);
   pinMode(PWMB, OUTPUT);
   pinMode(AIN1, OUTPUT);
   pinMode(BIN1, OUTPUT);
   
-  digitalWrite(PWMA, HIGH); // set speed of right motors to max
-  digitalWrite(PWMB, HIGH); // set speed of left motors to max
-  
-  delay(delayTime);
-}
-
-void forward() {
-  digitalWrite(AIN1, HIGH);  
-  digitalWrite(BIN1, HIGH);  
-}
-
-void backward() {
-  digitalWrite(AIN1, LOW);
-  digitalWrite(BIN1, LOW);
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
 }
 
 void moveRobot() {
   digitalWrite(STBY, HIGH);
-  delay(delayTime);
 }
 
-void stationary() {
+void stationary(float t) {
   digitalWrite(STBY, LOW);
-  delay(delayTime);
+  delay(t*1000); 
 }
 
-void left() {
+void forward(float d) {
+  moveRobot();
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(BIN1, HIGH); 
+  t = (d/2.45) * 1000; 
+  delay(t);  
+  stationary(1);
+}
+
+void backward(float d) {
+  moveRobot();
+  digitalWrite(AIN1, LOW);
+  digitalWrite(BIN1, LOW);
+  t = (d/2.45) * 1000; // rate = 2.45ft/sec
+  delay(t);  
+  stationary(1); 
+}
+
+void left(int deg) {
+  moveRobot();
   digitalWrite(AIN1, HIGH);
   digitalWrite(BIN1, LOW);
+  t = (deg/398.) * 1000;
+  delay(t);
+  stationary(1);
 }
 
-void right() {
+void right(int deg) {
+  moveRobot();
   digitalWrite(AIN1, LOW);
   digitalWrite(BIN1, HIGH);
-}
+  t = (deg/398) * 1000;
+  delay(t);
+  stationary(1);
+} 
 
 void loop() {
-  forward();
   moveRobot();
-  stationary();
+  left(90);
+  stationary(1); 
+  while(true) {
+    
   }
+}
